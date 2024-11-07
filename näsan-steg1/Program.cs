21f6a41c358e6472abd3885c1f34434571fba0aa
Signe Olofsson <signe.olofsson@elev.ga.ntig.se>
2024-11-07 13:09:47 +0100

    first commit

---
+++ näsan-steg1/Program.cs
@@ -0,0 +1,31 @@
+﻿// if(6>3){
+//     Console.WriteLine("Hello World"); 
+// }
+
+// Console.ReadLine(); 
+
+// Console.WriteLine("Skriv rätt användarnamn");
+// string noname; 
+
+// noname= Console.ReadLine();
+// string användarnamn = "kalleanka"; 
+
+// if(användarnamn == noname){
+//  Console.WriteLine("Välkomen"); 
+// }
+// else{
+//     Console.WriteLine("fel försök igen."); 
+// }
+
+// Console.ReadLine();
+
+Console.WriteLine("Hej skriv rätt användarnamn");
+string användarnamn = "kalleanka"; 
+string noname; 
+noname=Console.ReadLine(); 
+
+if(användarnamn== noname){
+    
+}
+
+Console.ReadLine();
